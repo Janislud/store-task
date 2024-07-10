4793cabdb7a8b6b8f2684b998d71722f5ecfa98b
Janislud <janisludborzs2@gmail.com>
2024-07-10 10:40:14 +0300

    create burger menu

--- resources/views/Components/navigation.blade.php
+++ resources/views/Components/navigation.blade.php
@@ -30,7 +30,15 @@
                     </a>
                 </button>
             </div>
+
+            <div class="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+
         </nav>
     </header>
+    <script src="./js/navbar.js"></script>
 </body>
 </html>
