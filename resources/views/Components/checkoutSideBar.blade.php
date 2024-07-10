3573eb8d1ae5e4ff6c654ecf18e1815ef72fc71c
alexander <yuriev200@gmail.com>
2024-07-10 14:40:24 +0300

    add checkout statgin basement

---
+++ resources/views/Components/checkoutSideBar.blade.php
@@ -0,0 +1,25 @@
+<div class="order">
+    <div class="order__content">
+        <div class="order__coupon">
+            <input class="order__coupon-input" type="text" placeholder="Coupon code">
+            <button class="order__coupon-addBtn" type="submit">Add code</button>
+        </div>
+        <div class="order__data">
+            <div>
+                <p class="order__text">
+                    Subtotal
+                </p>
+                <p class="order__bold">$ 9.99</p>
+            </div>
+            <div>
+                <p class="order__text">Shipping </p>
+                <p class="order__bold">{{$shipping}}</p>
+
+            </div>
+        </div>
+        <div class="order__total">
+            <p class="order__text">Total</p>
+            <p class="order__bold">{{$totalOrderPrice}}</p>
+        </div>
+    </div>
+</div>
