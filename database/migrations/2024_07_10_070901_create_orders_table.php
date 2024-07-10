0e2224c4916d7c967796feacd2b032a410c29ff9
avaiceulena <avaiceulena@gmail.com>
2024-07-10 13:31:39 +0300

    Migrations, Models

--- database/migrations/2024_07_10_070901_create_orders_table.php
+++ database/migrations/2024_07_10_070901_create_orders_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tests', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('total');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tests');
+        Schema::dropIfExists('orders');
     }
 };
