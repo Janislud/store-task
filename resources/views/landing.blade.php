17f2da7ee425da34ae927f1eec7732885f20be98
Janislud <janisludborzs2@gmail.com>
2024-07-11 09:30:27 +0300

    recreat ways

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,12 +1,3 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/style_landing.css') }}">
-    <title>Landing</title>
-</head>
-<body>
     <main>
         <section>
             <div class="background">
@@ -132,5 +123,3 @@
             </div>
         </section>
     </main>
-</body>
-</html>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="./css/products.css">
-        <title>Produt details page</title>
-    </head>
-    <body>
-    @include('Components.navigation')
-        <div class="main-wrapper">
-            <div class="container"></div>
-        </div>
-    @include('Components.footer')
-    </body>
-</html>
+@extends('layouts.layout')
+
+@section('title', 'About')
+
+@section('content')
+    <h1>Products</h1>
+@endsection
