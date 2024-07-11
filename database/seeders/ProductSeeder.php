a05d92e64d8126f4f3b1451dd7d634eec55e82a8
avaiceulena <avaiceulena@gmail.com>
2024-07-11 16:29:51 +0300

    data

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 use Faker\Factory as Faker;
 
 class ProductSeeder extends Seeder
