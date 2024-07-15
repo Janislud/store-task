84a8675040abddfd7d166558eaa1fa71e1ebc7f7
alexander <yuriev200@gmail.com>
2024-07-15 15:09:31 +0300

    add space between slides in pdp

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,5 +1,6 @@
 const swiper = new Swiper('.pdp__slider-wrapper.swiper', {
     loop: true,
+    spaceBetween: 20,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
