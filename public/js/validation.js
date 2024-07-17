dd968392cdbf99b486012fc23d05db6a71bae09b
alexander <yuriev200@gmail.com>
2024-07-17 15:54:43 +0300

    modify checkout validation now support unrequired input handle

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,6 +1,7 @@
 
 const validatePattern = {
-    email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    cardExpDate: /^(0[1-9]|1[0-2])\/\d{2}$/
 }
 
 if (window.location.pathname.split("/").includes("checkout")) {
@@ -9,10 +10,16 @@ if (window.location.pathname.split("/").includes("checkout")) {
 
     const submitBtn = document.querySelector(".stages button.stages__forward");
 
-    submitBtn.disabled = ![...allStageInputs].every(el => el.value);
+    submitBtn.disabled = ![...allStageInputs].every(el => el.value || !el.hasAttribute("required"));
 
     const validate = (input) => {
         let validated = true;
+
+        if (!input.hasAttribute("required")) {
+            validated = true;
+            input.dataset.validated = validated;
+            return validated
+        }
 
         if (input.value.length === 0) {
             validated = false;
@@ -21,12 +28,17 @@ if (window.location.pathname.split("/").includes("checkout")) {
             validated = validatePattern["email"].test(input.value);
         }
 
+        if (input.hasAttribute("cardExpDateInput")) {
+            validated = validatePattern["cardExpDate"].test(input.value);
+        }
+
         input.dataset.validated = validated;
 
         submitBtn.disabled = ![...allStageInputs].every(el => el.dataset.validated === "true" && el.dataset.untouched === "false");
     }
 
     allStageInputs.forEach(input => {
+
         if (input.value.length > 0) {
             input.dataset.untouched = "false";
             input.dataset.validated = "true"
@@ -34,8 +46,11 @@ if (window.location.pathname.split("/").includes("checkout")) {
             input.dataset.untouched = "true";
         }
 
+        if (!input.hasAttribute("required")) {
+            input.dataset.untouched = "false";
+        }
 
-        input.addEventListener("change", () => {
+        input.addEventListener("input", () => {
             input.dataset.untouched = "false";
         });
 
